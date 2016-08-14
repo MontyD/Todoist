14cb5768220e24ca696926b8fdcf226b3fe7a677
montyD <billy-da-fish-3rd@hotmail.com>
2016-08-14 16:26:26 +0100

    layout looking good

--- sockets/index.js
+++ sockets/index.js
@@ -1,15 +1,11 @@
 var socketsRouting = function(socket) {
     'use strict';
-    console.log('new user');
 
     socket.on('room', function(room) {
-      console.log('joining: ' + room);
-      socket.join(room);
-
-   });
-
-    socket.on('disconnect', function() {
-        console.log('user left');
+        // on connection socket echos room name,
+        // add socket to that name
+        console.log(room + ' being joined');
+        socket.join(room);
     });
 };
 
