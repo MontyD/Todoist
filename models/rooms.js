7e81401bfe74fe9cee0b5fceb0dc324becdc3839
montyD <billy-da-fish-3rd@hotmail.com>
2016-09-02 21:56:47 +0100

    passcode update woring

--- models/rooms.js
+++ models/rooms.js
@@ -3,6 +3,52 @@
 var bcrypt = require('bcrypt'),
     crypto = require('crypto');
 
+function generateHashAndSalt(room, options, cb) {
+    var done = 0,
+        passwordUpdate = false,
+        adminPasswordUpdate = false;
+    function complete() {
+        done--;
+        if (done === 0) {
+            cb(null, options);
+        }
+    }
+    if (options.fields.indexOf('password') > -1) {
+        done++;
+        bcrypt.genSalt(12, function(err, salt) {
+            if (err) {
+                return cb(err, options);
+            }
+            room.salt = salt;
+            bcrypt.hash(room.password, salt, function(err, hash) {
+                if (err) {
+                    return cb(err, options);
+                }
+                room.password = hash;
+                room.salt = salt;
+                return complete();
+            });
+        });
+    }
+    if (options.fields.indexOf('adminPassword') > -1) {
+        done++;
+        bcrypt.genSalt(12, function(err, salt) {
+            if (err) {
+                return cb(err, options);
+            }
+            room.adminSalt = salt;
+            bcrypt.hash(room.adminPassword, salt, function(err, hash) {
+                if (err) {
+                    return cb(err, options);
+                }
+                room.adminPassword = hash;
+                room.adminSalt = salt;
+                return complete();
+            });
+        });
+    }
+}
+
 module.exports = function(sequelize, DataTypes) {
     var room = sequelize.define('rooms', {
         name: {
@@ -22,8 +68,8 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false,
             validate: {
                 len: {
-                    args: [5, 40],
-                    msg: 'Please enter a password between five and 40 characters long'
+                    args: [5, 70],
+                    msg: 'Please enter a password between five and seventy characters long'
                 }
             }
         },
@@ -33,51 +79,16 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false,
             validate: {
                 len: {
-                    args: [5, 40],
-                    msg: 'Please enter a password between five and 40 characters long'
+                    args: [5, 70],
+                    msg: 'Please enter a password between five and seventy characters long'
                 }
             }
         },
         adminSalt: DataTypes.STRING
     }, {
         hooks: {
-            beforeCreate: function(room, options, cb) {
-                var done = 0;
-                function complete() {
-                    done++;
-                    if (done === 2) {
-                        cb(null, options);
-                    }
-                }
-                bcrypt.genSalt(12, function(err, salt) {
-                    if (err) {
-                        return cb(err, options);
-                    }
-                    room.salt = salt;
-                    bcrypt.hash(room.password, salt, function(err, hash) {
-                        if (err) {
-                            return cb(err, options);
-                        }
-                        room.password = hash;
-                        room.salt = salt;
-                        return complete();
-                    });
-                });
-                bcrypt.genSalt(12, function(err, salt) {
-                    if (err) {
-                        return cb(err, options);
-                    }
-                    room.adminSalt = salt;
-                    bcrypt.hash(room.adminPassword, salt, function(err, hash) {
-                        if (err) {
-                            return cb(err, options);
-                        }
-                        room.adminPassword = hash;
-                        room.adminSalt = salt;
-                        return complete();
-                    });
-                });
-            }
+            beforeCreate: generateHashAndSalt,
+            beforeUpdate: generateHashAndSalt
         }
     });
 
